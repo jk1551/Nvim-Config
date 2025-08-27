d6b23af0d35bf892b70ea5f639a973e7df609384
= <joe.k659@gmail.com>
2025-08-27 13:48:13 -0400

    treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,6 +19,20 @@ return {
       "folke/tokyonight.nvim",
       lazy = false,
       priority = 1000
+  },
+
+  {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function()
+          require("nvim-treesitter.configs").setup {
+              ensure_installed = {
+              "lua", "javascript", "typescript", "python", "c", "cpp", "rust", "json", "yaml", "html", "css", "c_sharp"
+              },
+              highlight = { enable = true },
+              indent = { enable = true },
+          }
+      end,
   }
 }
 
