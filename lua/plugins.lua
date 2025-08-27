54075d9ca78694b971b1c9f4731e1845a99a0f9f
= <joe.k659@gmail.com>
2025-08-27 09:18:16 -0400

    Nvim config

---
+++ lua/plugins.lua
@@ -0,0 +1,17 @@
+return {
+  -- Telescope fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  -- File explorer
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup()
+    end,
+  },
+}
+
