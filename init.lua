434ed4db84ef83cc7db1d642186d7db2397b3672
= <joe.k659@gmail.com>
2025-08-27 10:07:46 -0400

    add live grep

--- init.lua
+++ init.lua
@@ -28,6 +28,7 @@ require("lazy").setup("plugins")
 -- map some telescope keys
 
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
 
 -- file tree toggle
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
